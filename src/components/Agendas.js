ab68086dea913f9556aa017079d637cd69ae2636
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-11-27 12:54:55 -0600

    ajuste

--- src/components/Agendas.js
+++ src/components/Agendas.js
@@ -4,7 +4,7 @@ const agenda1 =
 const agenda2 =
   "https://firebasestorage.googleapis.com/v0/b/comite-coiner.appspot.com/o/agenda%2F26_NOV_AGENDA.png?alt=media&token=47dceb5d-b242-40a5-8919-b1943087418f";
 const agenda3 =
-  "https://firebasestorage.googleapis.com/v0/b/comite-coiner.appspot.com/o/agenda%2F27_NOV_AGENDA.png?alt=media&token=0d191d1f-4608-498c-b5b1-267a7a68b8fe";
+  "https://firebasestorage.googleapis.com/v0/b/comite-coiner.appspot.com/o/agenda%2FRecurso%202.png?alt=media&token=4da59ca6-8f3f-4fc2-a13c-dc5e1842e7cde";
 function Agendas() {
   return (
     <React.Fragment>
